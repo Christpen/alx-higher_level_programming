ee6e852c669c7fd015d16099f90b7e5e7d141ddb
Christpen <juniormncube78@gmail.com>
2024-03-11 21:56:47 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fristline = 'c is fun';
+const secondline = 'python is cool';
+const thirdline = 'JavaScript is amazing';
+console.log(fristline);
+console.log(secondline);
+console.log(thirdline);
+
